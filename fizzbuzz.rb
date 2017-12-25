d6b262674ed328d9ce3ccaaf9341f532865d9638
Pan L <limponkimo@gmail.com>
2017-12-25 05:52:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # if the number is evenly divisible by 3, it should return "Fizz", if it's divisible by 5, it should return "Buzz", and if it's divisible by both 3 and 5, it should return "FizzBuzz".
 
-def fizzbuzz(n)
+def fizzbuzz(number)
 if number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
   elsif number % 3 == 0
